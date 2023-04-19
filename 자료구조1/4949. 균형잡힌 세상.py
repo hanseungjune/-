00751a901b1ed64712f995cbd68bfecd7bfdcfdb
tmdwns950428 <tmdwns950428@naver.com>
2023-04-19 23:54:26 +0900

    20230419

--- 자료구조1/4949. 균형잡힌 세상.py
+++ 자료구조1/4949. 균형잡힌 세상.py
@@ -1,6 +1,31 @@
 while True:
-  str_input = list(input())
-  stack = []
-  for i in str_input:
-    if i in '([])':
-      pass
+  try: 
+    str_input = input()
+    if str_input == '.':
+      break
+    stack = []
+    for i in range(len(str_input)):
+      if str_input[i] not in '()[]':
+        continue
+      if str_input[i] == '(':
+        stack.append(str_input[i])
+      elif str_input[i] == '[':
+        stack.append(str_input[i])
+      elif str_input[i] == ')':
+        if not stack or stack[-1] != '(':
+          print('no')
+          break
+        stack.pop()
+      else:
+        if not stack or stack[-1] != '[':
+          print('no')
+          break
+        stack.pop()
+    else:
+      if not stack:
+        print('yes')
+      else:
+        print('no')
+  except Exception as e:
+    print(e)
+    break
