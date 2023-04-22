ae00b1ec7a555acc6f168a26aff58ec66a3c53a1
tmdwns950428 <tmdwns950428@naver.com>
2023-04-23 00:00:52 +0900

    20230423

--- 자료구조1/1021. 회전하는 큐.py
+++ 자료구조1/1021. 회전하는 큐.py
@@ -6,30 +6,19 @@ n, m = map(int, input().split())
 queue = deque(i for i in range(1, n+1))
 circle = list(map(int, input().split()))
 cnt = 0
-for c in range(len(circle)):
-  print(circle[c])
-  print(queue)
-  while True:
-    if circle[c] == queue[0]:
-      queue.popleft()
-      break
-    if len(queue)//2 % 2 == 1:
-      # 시계방향
-      if 0 < c <= len(queue)//2:
-        queue.appendleft(queue.pop())
-        cnt += 1
-      # 반시계방향
-      else:
-        queue.append(queue.popleft())
-        cnt += 1
-    else:
-      # 시계방향
-      if 0 < c <= len(queue)//2:
-        queue.appendleft(queue.pop())
-        cnt += 1
-      # 반시계방향
-      else:
-        queue.append(queue.popleft())
-        cnt += 1
-  print('카운트 ', cnt)
+
+for c in circle:
+    while True:
+        if queue[0] == c:
+            queue.popleft()
+            break
+        else:
+            if queue.index(c) < len(queue)/2:
+                while queue[0] != c:
+                    queue.append(queue.popleft())
+                    cnt += 1
+            else:
+                while queue[0] != c:
+                    queue.appendleft(queue.pop())
+                    cnt += 1
 print(cnt)
