685fd2f7fa15336e29f774fd9354e3386124134b
tmdwns950428 <tmdwns950428@naver.com>
2023-04-23 23:25:42 +0900

    20230423

--- 자료구조1/1918. 후위 표기식.py
+++ 자료구조1/1918. 후위 표기식.py
@@ -1,28 +1,32 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
+
 stack = deque()
-lst= deque(input().strip())
-res=''
+lst = deque(input().strip())
+res = ''
+
+# 우선순위 비교하려고
+priority = {'+': 1, '-': 1, '*': 2, '/': 2}
+
 for l in lst:
-  if l.isalpha():
-    res+=l
-  else:
-    if l == '(':
-      stack.append(l)
-    elif l == '*' or l =='/':
-      while stack and (stack[-1] == '*' or stack[-1] == '/'):
-        res += stack.pop()
-      stack.append(l)
-    elif l == '+' or l == '-':
-      while stack and stack[-1] != '(':
-        res += stack.pop()
-      stack.append(l)
+    if l.isalpha():
+        res += l
+    elif l == '(':
+        stack.append(l)
     elif l == ')':
-      while stack and stack[-1] != '(':
-        res += stack.pop()
-      stack.pop()
+        while stack and stack[-1] != '(':
+            res += stack.pop()
+        # '(' 이거 마지막에 빼려고 하는 거
+        stack.pop()
+    else:
+        # 우선순위가 크면 res에 바로 더하는게 맞음, 우선순위가 같거나 낮으면 그냥 스택으로 push
+        while stack and stack[-1] != '(' and priority[stack[-1]] >= priority[l]:
+            res += stack.pop()
+        stack.append(l)
+
 while stack:
-  res += stack.pop()
+    res += stack.pop()
+
 print(res)
-          
