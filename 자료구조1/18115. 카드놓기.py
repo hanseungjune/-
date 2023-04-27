acc0ef4d4d0ecff6f4757aebda365fa7827b3483
tmdwns950428 <tmdwns950428@naver.com>
2023-04-27 23:59:53 +0900

    20230427

--- 자료구조1/18115. 카드놓기.py
+++ 자료구조1/18115. 카드놓기.py
@@ -1,14 +1,17 @@
+import sys
 from collections import deque
-n = int(input())
 
-# 제일 위의 카드 버리기
-# 위에서 2번째 카드 버리기
-# 제일 밑에 있는 카드 버리기
+n = int(sys.stdin.readline())
+li = list(map(int, sys.stdin.readline().split()))[::-1]
 
-lst = list(map(int, input().split()))[::-1]
-floor = [i for i in range(n, 1, -1)]
-print(lst)
-print(floor)
-front= [1]
 dq = deque()
-# for command in lst[1:]:
+for i in range(n):
+  if li[i] == 1:
+    dq.appendleft(i + 1)
+  elif li[i] == 2:
+    dq.insert(1, i+1)
+  elif li[i] == 3:
+    dq.append(i+1)
+
+for i in dq:
+  print(i, end=' ')
